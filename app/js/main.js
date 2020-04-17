c485745ac16d5305c7239b9dd5982fddd2a7168b
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2020-04-17 23:11:59 +0300

    mini update

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,17 @@
+window.onscroll = function headerShow(){
+
+  var header__top = document.querySelector('.header__top');
+  var header__main = document.querySelector('.header-main__box');
+
+  if(window.pageYOffset > 150){
+    header__top.classList.add('header--fixed');
+    header__main.classList.add('header-main__box--fix');
+  } else if(window.pageYOffset < 1){
+    header__top.classList.remove('header--fixed');
+    header__main.classList.remove('header-main__box--fix');
+  }
+}
+
 
 $(document).ready(function(){
     $('.skins__slider').slick({
