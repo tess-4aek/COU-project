fd46d2115dbeae149ed98ff0ac1bb96df803dbf1
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2020-04-18 20:33:04 +0300

    preloader

--- app/js/main.js
+++ app/js/main.js
@@ -1,22 +1,37 @@
-window.onscroll = function headerShow(){
+
+window.onload = function preload() { 
+  var html = document.querySelector('html');
+  html.classList.add('html--hiden');
+  setTimeout(function(){
+    var preloader = document.getElementById('preloader');
+    preloader.style.display = 'none';
+    html.classList.remove('html--hiden');
+  }, 5000);
+};
+
+
+
+
+
+window.onscroll = function headerShow() {
 
   var header__top = document.querySelector('.header__top');
   var header__main = document.querySelector('.header-main__box');
 
-  if(window.pageYOffset > 150){
+  if (window.pageYOffset > 150) {
     header__top.classList.add('header--fixed');
     header__main.classList.add('header-main__box--fix');
-  } else if(window.pageYOffset < 1){
+  } else if (window.pageYOffset < 1) {
     header__top.classList.remove('header--fixed');
     header__main.classList.remove('header-main__box--fix');
   }
-}
+};
 
 
-$(document).ready(function(){
-    $('.skins__slider').slick({
-      autoplay: true,
-      autoplaySpeed: 2500,
-    }
-    );
-  });
+$(document).ready(function () {
+  $('.skins__slider').slick({
+    autoplay: true,
+    autoplaySpeed: 2500,
+  }
+  );
+});
