1735c8cba1e407c96f53d2f410ab3f2675ab682c
tess-4aek <56252678+tess-4aek@users.noreply.github.com>
2020-05-31 16:56:49 +0300

    fix 05.31.20

--- app/js/main.js
+++ app/js/main.js
@@ -15,20 +15,20 @@ window.onscroll = function headerShow() {
 
   var header__top = document.querySelector('.header__top');
   var header__main = document.querySelector('.header-main__box');
-  var header__mobi = document.querySelector('.header__top-mobile');
+  var header__mobi = document.querySelector('.header__top-mobile'); 
 
   if (window.innerWidth > 800 &  window.pageYOffset > 150) {
     header__top.classList.add('header--fixed');
-    
+    header__main.classList.add('header-main__box--fixed');
   } else if (window.innerWidth > 800 & window.pageYOffset < 1) {
     header__top.classList.remove('header--fixed');
-    
+    header__main.classList.remove('header-main__box--fixed');
   } else if (window.innerWidth <= 800 &  window.pageYOffset > 70){
     header__mobi.classList.add('header__top-mobile--fixed');
-    
+    header__main.classList.add('header-main__box--fixed-mobile');
   } else if (window.innerWidth <= 800 &  window.pageYOffset < 1){
     header__mobi.classList.remove('header__top-mobile--fixed');
-    
+    header__main.classList.remove('header-main__box--fixed-mobile');
   }
 };
 
